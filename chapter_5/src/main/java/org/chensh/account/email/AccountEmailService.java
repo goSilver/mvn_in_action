02d44ebcd4f073b4c498df88039945059bce4eeb
chensh <silverchen961@foxmail.com>
2023-07-15 22:44:41 +0800

    add:account email

---
+++ chapter_5/src/main/java/org/chensh/account/email/AccountEmailService.java
@@ -0,0 +1,7 @@
+package org.chensh.account.email;
+
+public interface AccountEmailService
+{
+    void sendMail( String to, String subject, String htmlText )
+        throws AccountEmailException;
+}
